2e86cf1d0e5335eb1ffee0065382e3fc6f15e671
Naga Deepika Sreeghakollapu <42428713+sndeepika@users.noreply.github.com>
2020-07-09 10:49:37 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/mentalhealth'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db = SQLAlchemy(app)
+
+class Example(db.Model):
+	__tablename__ = 'example'
+	id = db.Column('id', db.Integer, primary_key=True)
+	data = db.Column('data', db.String(100))
+
+@app.route("/")
+def main():
+    return "Welcome!"
+if __name__ == "__main__":
+    app.run()
