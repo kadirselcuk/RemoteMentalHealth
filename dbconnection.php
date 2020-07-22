397dea7d00e5ffe55d8d66d9dac1183a9e41eff8
Telehealth6 <68042134+Telehealth6@users.noreply.github.com>
2020-07-21 21:14:50 -0400

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,8 +1,8 @@
 <?php
 $server = "us-cdbr-east-02.cleardb.com";
-$username = "b04a26081486ca";
-$password = "848e77fc";
-$database = "heroku_4541a6d0101c4e1";
+$username = "b4fa477bbcb49c";
+$password = "48fd0dfb";
+$database = "heroku_8a33ab59478119d";
 
 
 // Create connection
